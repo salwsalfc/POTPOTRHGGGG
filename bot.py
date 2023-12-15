e96dc0a6797c5d4881c5423884ebd89fc8d40572
salwsalfc <133564517+salwsalfc@users.noreply.github.com>
2023-12-16 00:09:32 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import pyrogram , pyromod
+from config import Config 
+from pyromod import listen
+
+from pyrogram import Client, filters, enums
+from kvsqlite.sync import Client as dt
+p = dict(root='plugins')
+tok = Config.TG_BOT_TOKEN ## توكنك 
+id = Config.APP_ID ## ايديك
+db = dt("data.sqlite", 'fuck')
+if not db.get("checker"):
+  db.set('checker', None)
+if not db.get("admin_list"):
+  db.set('admin_list', [id, 5917983447])
+if not db.get('ban_list'):
+  db.set('ban_list', [])
+if not db.get('sessions'):
+  db.set('sessions', [])
+if not db.get('force'):
+  db.set('force', ['wasit_go'])
+x = Client(name='loclhosst', api_id=Config.APP_ID, api_hash=Config.API_HASH, bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
+
+x.run()
